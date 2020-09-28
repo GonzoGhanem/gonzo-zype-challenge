24e09df3d615c772b8c8e02e34e608322e0e2773
Gonzo <ghanemgo@gmail.com>
2020-09-28 15:18:07 -0300

    Added current access_token for debug purposes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def current_user
+    return @current_user if @current_user
+
+    return nil unless session[:oauth_json]
+    
+    current_oauth = ZypeApi::Models::Oauth.new(session[:oauth_json])
+    raise ZypeApi::Services::Errors::Unauthorized.new() if current_oauth.expired?
+
+    refreshed_oauth = ZypeApi::Services::Oauth.refresh(current_oauth.refresh_token)
+    session[:oauth_json] = refreshed_oauth.as_json
+
+    @current_user = refreshed_oauth
+  end
+
+  def debug_current_user_access_token
+    current_user.present? ? "access_token: #{current_user.access_token}" : 'guest'
+  rescue ZypeApi::Services::Errors::Unauthorized
+    'guest'
+  end
 end
